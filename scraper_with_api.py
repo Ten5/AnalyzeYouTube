f509eacce27fa47d7f6df39ed34f97b40fa0df1a
Subhayu Chakravorty <sbhchakravorty@gmail.com>
2021-03-17 00:19:33 -0700

    Youtube Data API v3.

---
+++ scraper_with_api.py
@@ -0,0 +1,141 @@
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+import urllib.parse as p
+import re
+import os
+import pickle
+
+SCOPES = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+
+
+###################################
+### Authenticating with YouTube ###
+###################################
+def youtube_authenticate():
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "credentials.json"
+    creds = None
+    # the file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first time
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+    # if there are no (valid) credentials availablle, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # save the credentials for the next run
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+
+    return build(api_service_name, api_version, credentials=creds)
+
+# authenticate to YouTube API
+youtube = youtube_authenticate()
+
+
+
+###################################
+###### Getting video details ######
+###################################
+def get_video_id_by_url(url):
+    """
+    Return the Video ID from the video `url`
+    """
+    # split URL parts
+    parsed_url = p.urlparse(url)
+    # get the video ID by parsing the query of the URL
+    video_id = p.parse_qs(parsed_url.query).get("v")
+    if video_id:
+        return video_id[0]
+    else:
+        raise Exception(f"Wasn't able to parse video URL: {url}")
+
+def get_video_details(youtube, **kwargs):
+    return youtube.videos().list(
+        part="snippet,contentDetails,statistics",
+        **kwargs
+    ).execute()
+
+def print_video_infos(video_response):
+    items = video_response.get("items")[0]
+    # get the snippet, statistics & content details from the video response
+    snippet         = items["snippet"]
+    statistics      = items["statistics"]
+    content_details = items["contentDetails"]
+    # get infos from the snippet
+    channel_title = snippet["channelTitle"]
+    title         = snippet["title"]
+    description   = snippet["description"]
+    publish_time  = snippet["publishedAt"]
+    # get stats infos
+    comment_count = statistics["commentCount"]
+    like_count    = statistics["likeCount"]
+    dislike_count = statistics["dislikeCount"]
+    view_count    = statistics["viewCount"]
+    # get duration from content details
+    duration = content_details["duration"]
+    # duration in the form of something like 'PT5H50M15S'
+    # parsing it to be something like '5:50:15'
+    parsed_duration = re.search(f"PT(\d+H)?(\d+M)?(\d+S)", duration).groups()
+    duration_str = ""
+    for d in parsed_duration:
+        if d:
+            duration_str += f"{d[:-1]}:"
+    duration_str = duration_str.strip(":")
+    print(f"""\
+    Title: {title}
+    Description: {description}
+    Channel Title: {channel_title}
+    Publish time: {publish_time}
+    Duration: {duration_str}
+    Number of comments: {comment_count}
+    Number of likes: {like_count}
+    Number of dislikes: {dislike_count}
+    Number of views: {view_count}
+    """)
+
+
+# Search by keywords
+def search(youtube, **kwargs):
+    return youtube.search().list(
+        part="snippet",
+        **kwargs
+    ).execute()
+
+
+
+###################################
+############# Testing #############
+###################################
+video_url = "https://www.youtube.com/watch?v=jNQXAC9IVRw&ab_channel=jawed"
+# parse video ID from URL
+video_id = get_video_id_by_url(video_url)
+# make API call to get video info
+response = get_video_details(youtube, id=video_id)
+# print extracted video infos
+print_video_infos(response)
+
+
+
+
+
+####################################
+############## Output ##############
+####################################
+# Title: Me at the zoo
+# Description: The first video on YouTube. Maybe it's time to go back to the zoo?
+# Channel Title: jawed
+# Publish time: 2005-04-24T03:31:52Z
+# Duration: 19
+# Number of comments: 11018071
+# Number of likes: 5962957
+# Number of dislikes: 153444
+# Number of views: 138108884
